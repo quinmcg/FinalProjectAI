83edf55ce963097b850be036210d9827b54f2b47
Quinlan McGaugh <jmcgaugh@middlebury.edu>
2022-05-20 14:02:28 -0400

    Temp Commit

--- main.py
+++ main.py
@@ -4,35 +4,59 @@ import numpy as np
 import csv
 import pandas as pd
 import matplotlib
+from tabulate import tabulate
 
-class Main:
+# List of Tuning Parameters as Global Variables (To Do/Decide on)
+# Global so that we keep them constant for all decision trees
 
-    def decisionTree():
-        # with open("relationshipstatus.csv", mode = 'r') as file_name:
-        #     trainclassification = csv.reader(file_name)
-        #
-        # with open("trainingfeatures.csv", mode = 'r') as file_name:
-        #     trainfeatures = csv.reader(file_name)
-        # for lines in trainfeatures:
-        #     print(lines)
+minSplit = 50
+numtrees = 1
 
-        trainclassification = pd.read_csv("relationshipstatus.csv")
-        trainfeatures = pd.read_csv("trainingfeatures.csv")
-        #find_partner	gender	gpa	class	siblings	parents_married	race	housing	year
-        trainfeatures_dummy = pd.get_dummies(trainfeatures, columns = ['find_partner', 'gender', 'class', 'parents_married', 'race', 'housing', 'year'])
-        featuresarray = trainfeatures_dummy.to_numpy()
-        classifarray = trainclassification.to_numpy()
-        #print(trainfeatures_dummy.loc[0])
+class DecisionTree:
 
-        classifier = tree.DecisionTreeClassifier(min_samples_split = 50)
-        classifier.fit(featuresarray, classifarray)
+    def __init__(self, features, classification):
+        self.features = features
+        self.classification = classification
+        self.classifier = tree.DecisionTreeClassifier(min_samples_split = minSplit)
 
-        tree.plot_tree(classifier)
-        dot_data = tree.export_graphviz(classifier, out_file=None)
+    def buildTree(self):
+        self.trainfeatures_dummy = pd.get_dummies(self.features)
+        self.featuresarray = self.trainfeatures_dummy.to_numpy()
+        self.classifarray = self.classification.to_numpy()
+        self.classifier.fit(self.featuresarray, self.classifarray)
+
+    def renderTree(self):
+        tree.plot_tree(self.classifier)
+        dot_data = tree.export_graphviz(self.classifier, out_file=None)
         graph = graphviz.Source(dot_data)
         graph.render("midd")
 
+
+
+
 if __name__ == '__main__':
 
+        trainclassification = pd.read_csv("relationshipstatus.csv")
+        trainfeatures = pd.read_csv("trainingfeatures.csv")
 
-    Main.decisionTree()
+        #print(trainfeatures.to_markdown())
+
+        newtree = DecisionTree(trainfeatures, trainclassification)
+        newtree.buildTree()
+        newtree.renderTree()
+
+        for i in range(numtrees):
+            #sample our data
+            tempclassification = pd.DataFrame()
+            tempfeatures = pd.DataFrame()
+
+            for r in range(len(trainclassification)):
+                index = np.random.randint(0, len(trainclassification))
+                currfeatures = trainfeatures.loc[index]
+                currclass = trainclassification.loc[index]
+                tempclassification = pd.concat([tempclassification, currclass])
+                tempfeatures = pd.concat([tempfeatures, currfeatures])
+
+            print(tempfeatures.to_markdown())
+
+            newtree = DecisionTree(tempfeatures, tempclassification)
